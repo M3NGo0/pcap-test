acae06de302f65a0574afab3a485ba7eff67d5f0
M3NGo0 <ddalgicake2000@naver.com>
2023-07-07 18:10:37 +0900

    Update All

---
+++ pcap-test.c
@@ -0,0 +1,81 @@
+#include <pcap.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+#define ETHER_ADDR_LEN 6
+#define ETHER_TYPE_IP 0x800
+
+struct libnet_ethernet_hdr
+{
+    u_int8_t  ether_dhost[ETHER_ADDR_LEN];/* destination ethernet address */
+    u_int8_t  ether_shost[ETHER_ADDR_LEN];/* source ethernet address */
+    u_int16_t ether_type;                 /* protocol */
+};
+
+void printMac(uint8_t* m) {
+	printf("%02x:%02x:%02x:%02x:%02x:%02x",m[0],m[1],m[2],m[3],m[4],m[5]);
+}
+
+void usage() {
+	printf("syntax: pcap-test <interface>\n");
+	printf("sample: pcap-test wlan0\n");
+}
+
+typedef struct {
+	char* dev_;
+} Param;
+
+
+
+Param param = {
+	.dev_ = NULL
+};
+
+bool parse(Param* param, int argc, char* argv[]) {
+	if (argc != 2) {
+		usage();
+		return false;
+	}
+	param->dev_ = argv[1];
+	return true;
+}
+
+int main(int argc, char* argv[]) {
+	if (!parse(&param, argc, argv))
+		return -1;
+
+	char errbuf[PCAP_ERRBUF_SIZE];
+	pcap_t* pcap = pcap_open_live(param.dev_, BUFSIZ, 1, 1000, errbuf);
+	if (pcap == NULL) {
+		fprintf(stderr, "pcap_open_live(%s) return null - %s\n", param.dev_, errbuf);
+		return -1;
+	}
+
+	while (true) {
+		struct pcap_pkthdr* header;
+		const u_char* packet;
+		int res = pcap_next_ex(pcap, &header, &packet);
+		if (res == 0) continue;
+		if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
+			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
+			break;
+		}
+		printf("%u bytes captured\n", header->caplen);
+		struct libnet_ethernet_hdr* eth_hdr= (struct libnet_ethernet_hdr*) packet;
+
+		printMac(eth_hdr->ether_shost);
+		printf(" ");
+		printMac(eth_hdr->ether_dhost);
+		printf("\n");
+
+		if(ntohs(eth_hdr->ether_type)!=ETHER_TYPE_IP) {
+			continue;
+			
+		} else {
+
+		}
+
+
+		pcap_close(pcap);
+	}
+}
